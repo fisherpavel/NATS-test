f63d95c4081825f899f0dde318c3a22688d4b4d9
fisherpavel <fisherpavel@bk.ru>
2020-08-03 11:53:14 +0700

    init project

---
+++ src/publisher.ts
@@ -0,0 +1,34 @@
+import nats from 'node-nats-streaming'
+import {TicketCreatedPublisher} from './events/ticket-created-publisher'
+
+console.clear()
+
+const stan = nats.connect('ticket', 'asdf', {
+    url: 'http://localhost:4222'
+})
+
+stan.on('connect', async () => {
+    console.log('Publisher connected to NATS')
+
+    const publisher = new TicketCreatedPublisher(stan)
+    try {
+        await publisher.publish({
+            id: '123',
+            title: 'concert',
+            price: 1233
+        })
+    } catch(err){
+        console.error(err)
+    }
+   
+
+    // const data = JSON.stringify({
+    //     id: '123',
+    //     title: 'concert',
+    //     price: 1233
+    // })
+
+    // stan.publish('ticket:created', data, () => {
+    //     console.log('Event published')
+    // })
+})
